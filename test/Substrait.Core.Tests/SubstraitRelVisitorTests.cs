4445c4d900867e4364ca6be8a09b4366f06cc68f
Anas Ismail Khan <anasismail28@gmail.com>
2026-08-02 18:42:34 +0500

    migrate sln to slnx, add test project, add ci workflow

---
+++ test/Substrait.Core.Tests/SubstraitRelVisitorTests.cs
@@ -0,0 +1,27 @@
+﻿using Substrait.Core;
+using Substrait.Relation;
+
+namespace Substrait.Core.Tests;
+
+public class SubstraitRelVisitorTests
+{
+  private sealed class NoopVisitor : SubstraitRelVisitor
+  {
+  }
+
+  [Fact]
+  public void Visit_WithNullRelation_ThrowsArgumentNullException()
+  {
+    var visitor = new NoopVisitor();
+
+    Assert.Throws<ArgumentNullException>(() => visitor.Visit((Read)null!));
+  }
+
+  [Fact]
+  public void Visit_WithUnhandledRelation_FallsBackAndThrows()
+  {
+    var visitor = new NoopVisitor();
+
+    Assert.Throws<InvalidOperationException>(() => visitor.Visit(new Read()));
+  }
+}
