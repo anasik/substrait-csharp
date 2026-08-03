9af92ad4a55a6c9def6c6f6c910a262227ccb8c3
Anas Ismail Khan <anasismail28@gmail.com>
2026-08-03 15:12:51 +0500

    Filescoped namespace and fixed import

--- src/Substrait.Core/SubstraitRelVisitor.cs
+++ src/Substrait.Core/SubstraitRelVisitor.cs
@@ -1,107 +1,106 @@
-﻿using Substrait.Relation;
+﻿using Substrait.Core.Relation;
 
-namespace Substrait.Core
+namespace Substrait.Core;
+
+/// <summary>
+/// Visitor to transform, compile, and/or process SQL logical operators represented using Substrait. The visitor has
+/// methods for visiting relation operator objects as input and provides concrete implementation to meet its goals.
+/// </summary>
+public abstract class SubstraitRelVisitor
 {
   /// <summary>
-  /// Visitor to transform, compile, and/or process SQL logical operators represented using Substrait. The visitor has
-  /// methods for visiting relation operator objects as input and provides concrete implementation to meet its goals.
+  /// Visit relational operator of type AGGREGATE
   /// </summary>
-  public abstract class SubstraitRelVisitor
+  public void Visit(Aggregate aggregate)
   {
-    /// <summary>
-    /// Visit relational operator of type AGGREGATE
-    /// </summary>
-    public void Visit(Aggregate aggregate)
+    if (aggregate is null)
     {
-      if (aggregate is null)
-      {
-        throw new ArgumentNullException(nameof(aggregate));
-      }
-
-      Fallback(aggregate);
+      throw new ArgumentNullException(nameof(aggregate));
     }
 
-    /// <summary>
-    /// Visit relational operator of type FETCH
-    /// </summary>
-    public void Visit(Fetch fetch)
-    {
-      if (fetch is null)
-      {
-        throw new ArgumentNullException(nameof(fetch));
-      }
+    Fallback(aggregate);
+  }
 
-      Fallback(fetch);
+  /// <summary>
+  /// Visit relational operator of type FETCH
+  /// </summary>
+  public void Visit(Fetch fetch)
+  {
+    if (fetch is null)
+    {
+      throw new ArgumentNullException(nameof(fetch));
     }
 
-    /// <summary>
-    /// Visit relational operator of type FILTER
-    /// </summary>
-    public void Visit(Filter filter)
-    {
-      if (filter is null)
-      {
-        throw new ArgumentNullException(nameof(filter));
-      }
+    Fallback(fetch);
+  }
 
-      Fallback(filter);
+  /// <summary>
+  /// Visit relational operator of type FILTER
+  /// </summary>
+  public void Visit(Filter filter)
+  {
+    if (filter is null)
+    {
+      throw new ArgumentNullException(nameof(filter));
     }
 
-    /// <summary>
-    /// Visit relational operator of type JOIN
-    /// </summary>
-    public void Visit(Join join)
-    {
-      if (join is null)
-      {
-        throw new ArgumentNullException(nameof(join));
-      }
+    Fallback(filter);
+  }
 
-      Fallback(join);
+  /// <summary>
+  /// Visit relational operator of type JOIN
+  /// </summary>
+  public void Visit(Join join)
+  {
+    if (join is null)
+    {
+      throw new ArgumentNullException(nameof(join));
     }
 
-    /// <summary>
-    /// Visit relational operator of type PROJECT
-    /// </summary>
-    public void Visit(Project project)
-    {
-      if (project is null)
-      {
-        throw new ArgumentNullException(nameof(project));
-      }
+    Fallback(join);
+  }
 
-      Fallback(project);
+  /// <summary>
+  /// Visit relational operator of type PROJECT
+  /// </summary>
+  public void Visit(Project project)
+  {
+    if (project is null)
+    {
+      throw new ArgumentNullException(nameof(project));
     }
 
-    /// <summary>
-    /// Visit relational operator of type READ
-    /// </summary>
-    public void Visit(Read read)
-    {
-      if (read is null)
-      {
-        throw new ArgumentNullException(nameof(read));
-      }
+    Fallback(project);
+  }
 
-      Fallback(read);
+  /// <summary>
+  /// Visit relational operator of type READ
+  /// </summary>
+  public void Visit(Read read)
+  {
+    if (read is null)
+    {
+      throw new ArgumentNullException(nameof(read));
     }
 
-    /// <summary>
-    /// Visit relational operator of type SORT
-    /// </summary>
-    public void Visit(Sort sort)
-    {
-      if (sort is null)
-      {
-        throw new ArgumentNullException(nameof(sort));
-      }
+    Fallback(read);
+  }
 
-      Fallback(sort);
+  /// <summary>
+  /// Visit relational operator of type SORT
+  /// </summary>
+  public void Visit(Sort sort)
+  {
+    if (sort is null)
+    {
+      throw new ArgumentNullException(nameof(sort));
     }
 
-    public void Fallback(Rel _)
-    {
-      throw new InvalidOperationException();
-    }
+    Fallback(sort);
+  }
+
+  public void Fallback(Rel _)
+  {
+    throw new InvalidOperationException();
   }
 }
